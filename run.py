aa4871588d7be5412c349e479b72ed8aefde5828
Shorouq <shorouq.zahra@ri.se>
2025-04-26 11:08:14 +0200

    refactor run.py

--- run.py
+++ run.py
@@ -3,21 +3,57 @@ from src.utils import scrape_text_from_url, extract_products
 import openai
 from dotenv import load_dotenv
 import os
-from pprint import pprint 
+import argparse
+
 load_dotenv()
 
-client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-model = "gpt-4o-2024-08-06"
-res = search("CDB gummies for sleep", num_results=200, unique=True, lang="en", region="us")
-output_dir = "output"
-prompt = "Extract information on each CBD gummy product you can find in this text from a website. Capture the name of the `brand`, the name of the `product`, a url to the lab report if it's available, and whether or not it's a product page."
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-m",
+    "--model",
+    dest="model",
+    help="OpenAI model",
+    type=str,
+    default="gpt-4.1",
+    required=False,
+)
+parser.add_argument(
+    "-p",
+    "--prompt",
+    dest="prompt",
+    help="Prompt to the system.",
+    type=str,
+    required=False,
+    default="Extract information on each CBD gummy product you can find in this text from a website. Capture the name of the `brand`, the name of the `product`, a url to the lab report if it's available, and whether or not it's a product page."
+)
+parser.add_argument(
+    "-o",
+    "--output_dir",
+    dest="output_dir",
+    help="Output directory",
+    type=str,
+    required=True,
+)
 
+parser.add_argument(
+    "-s",
+    "--search",
+    dest="search",
+    help="Search query for Google.",
+    type=str,
+    required=False,
+    default="CBD gummies for sleep"
+)
+
+args = parser.parse_args()
+client = openai.OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+res = search(
+    args.search, num_results=200, unique=True, lang="en", region="us"
+)
 for i in res:
     text = scrape_text_from_url(i)
     print("----")
     print(i)
-    products = extract_products(text, prompt, model, client)
+    products = extract_products(text, args.prompt, args.model, client)
     print("----")
-
-
-
